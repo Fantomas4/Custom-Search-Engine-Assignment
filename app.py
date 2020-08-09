b8181525b1ebdce66cd5bed9b4522c7bd67ef9ee
Fantomas4 <Fantomas4@users.noreply.github.com>
2020-08-09 17:34:32 +0300

    Made minor changes to front-end code.

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
-class Webpage: #this object is keeping all the info for printing
-    def __init__(self,title,url,similarity):
-        self.title=title
-        self.url=url
-        self.similarity=similarity
+
+# Class used to represent the query results we want printed.
+class Webpage:
+    def __init__(self, title, url, similarity):
+        self.title = title
+        self.url = url
+        self.similarity = similarity
 
 
-@app.route("/",methods=['POST','GET'])
+@app.route("/", methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        query=request.form['query']
+        query = request.form['query']
         #transform query
         #make the question to inverse catalog
-        webpage=Webpage(title="This is a huge title",url="https://www.facebook.com",similarity=0.7)
-        return render_template("index.html",webpages=[webpage])
+        webpage = Webpage(title="This is a huge title", url="https://www.facebook.com", similarity=0.7)
+        return render_template("index.html", webpages=[webpage])
     else:
         return render_template("index.html")
 
