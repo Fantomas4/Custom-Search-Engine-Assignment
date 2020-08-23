1d7367cf52342afec7134e50a8412558e28d0139
Fantomas4 <Fantomas4@users.noreply.github.com>
2020-08-23 04:27:56 +0300

    Made changes to UI appearance (HTML + CSS).

--- app.py
+++ app.py
@@ -2,21 +2,13 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 
-# Class used to represent the query results we want printed.
-class Webpage:
-    def __init__(self, title, url, similarity):
-        self.title = title
-        self.url = url
-        self.similarity = similarity
-
-
 @app.route("/", methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         query = request.form['query']
         #transform query
         #make the question to inverse catalog.
-        webpage = Webpage(title="This is a huge title", url="https://www.facebook.com", similarity=0.7)
+        webpage = {"title": "This is a huge title", "url": "https://www.facebook.com"}
         return render_template("index.html", webpages=[webpage])
     else:
         return render_template("index.html")
